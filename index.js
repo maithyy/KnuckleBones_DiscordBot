26b1d40548ef80b1d94f1f8e61e86124a800c476
Maithy Le <maithyle265@yahoo.com>
2022-09-04 22:11:15 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(token);
